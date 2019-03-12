8a70085e8b3dec4a6809069cb41db43ec8005271
Holly Emma Johnson <hollyjnz@gmail.com>
2019-03-12 16:58:23 +1300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <MyHeader/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MyHeader from "./components/MyHeader";
+
+export default {
+  name: "App",
+  components: {
+    MyHeader
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+    <nav class="nav">check</nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyHeader"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
